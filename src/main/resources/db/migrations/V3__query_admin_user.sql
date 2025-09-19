9018873ce9253b3fa9c8ce7b26945ddca4aa3cd8
Robertooanavas07 <RM551460@fiap.com.br>
2025-09-19 02:22:03 -0300

    possivel conserto do erro

--- src/main/resources/db/migrations/V3__query_admin_user.sql
+++ src/main/resources/db/migrations/V3__query_admin_user.sql
@@ -1,19 +1,19 @@
 INSERT INTO USERS (
-    ID_USER,
-    CLIENT_NAME,
-    EMAIL,
-    PASSWORD,
-    USER_PIX_KEY,
-    USERNAME,
-    BET_MAX_VALUE,
+    ID_USER, 
+    CLIENT_NAME, 
+    EMAIL, 
+    PASSWORD, 
+    USER_PIX_KEY, 
+    USERNAME, 
+    BET_MAX_VALUE, 
     REGISTER_DATE
 ) VALUES (
-    200,
-    'Teste DAST',
-    'teste@dominio.com',
-    '$2a$10$TgN7jG7Xx0yP4Q/okF7b7eV/5S1Rj4OX4P8g9Ej7N4Rt4s5lFQ2Cu',
-    'teste-pix',
-    'teste',
-    1000,
+    100, 
+    'Administrador', 
+    'admin@dominio.com', 
+    '$2a$10$PEHLAhyE/9sqNxkB6cup/.iOiVfdPPD8vwVmLGRKQj4AzWt1O9PsK',  
+    'admin-pix-chave', 
+    'admin', 
+    10000.00, 
     CURRENT_TIMESTAMP
 );
